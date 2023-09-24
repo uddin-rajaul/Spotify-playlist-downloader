0906c99f8d3bff9b8d4cd0fff49ba7e530942499
Rajaul <uddin.rajaul1@gmail.com>
2023-09-25 00:42:27 +0545

    added try except for to solve the error

--- spotify_playlist_download.py
+++ spotify_playlist_download.py
@@ -119,15 +119,17 @@ def extract_spotify_playlist_songs(playlist_url, client_id, client_secret, redir
 if __name__ == "__main__":
     client_id, client_secret, redirect_uri = get_api_credentials()
 
-    output_directory = "C:/Users/uddin/Downloads/Music/Trouvaille"  # Replace with your desired directory
+    output_directory = "C:/Users/uddin/Downloads/Music/IU"  # Replace with your desired directory
     playlist_url = input("Enter playlist URL: ")
     song_titles = extract_spotify_playlist_songs(playlist_url,client_id, client_secret, redirect_uri)
     
     # Now you have a list of song titles that you can iterate over or process further
     for song_title in song_titles:
-        print(song_title)
-        first_link = extract_first_youtube_search_link(song_title)
-        if first_link:
-            download_video_and_convert_to_mp3(first_link, output_directory)
-        else:
-            print("No link found.")
+        try:
+            first_link = extract_first_youtube_search_link(song_title)
+            if first_link:
+                download_video_and_convert_to_mp3(first_link, output_directory)
+            else:
+                print("No link found.")
+        except Exception as e:
+            print(f"Error processing song '{song_title}': {str(e)}")
